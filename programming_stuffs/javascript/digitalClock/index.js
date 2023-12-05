6cf9f5bde8bce3697d55e0851b6d366747cf1c4e
DDevil NightRaid <ddevil@gmail.com>
2023-12-05 19:35:16 +0545

    digital clock first demo of js

---
+++ programming_stuffs/javascript/digitalClock/index.js
@@ -0,0 +1,13 @@
+function updateClock(){
+    let currentdate = new Date();
+    let hours = currentdate.getHours()
+    let minutes = currentdate.getMinutes()
+    let seconds = currentdate.getSeconds()
+
+    document.getElementById('hour').innerHTML = hours;
+    document.getElementById('minute').innerHTML = minutes;
+    document.getElementById('second').innerHTML = seconds;
+
+}
+setInterval(updateClock, 1000);
+updateClock()
